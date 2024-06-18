4c4dbdb6f4a85b226c17255f09ca97a2401f2da4
EmmaBia93 <93839681+EmmaBia93@users.noreply.github.com>
2024-06-18 12:43:03 -0300

    Commit 003: Conexión con el Front

--- server/app/__init__.py
+++ server/app/__init__.py
@@ -1,5 +1,6 @@
 
 from flask import Flask
+from flask_cors import CORS
 from app.config import Config
 from app.extensions import db
 from app.models.book import Book
@@ -8,13 +9,14 @@ from app.models.category import Category
 from app.routes.main import main_bp
 
 
+
 def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
     
     # Inicializar extensiones
     db.init_app(app)
-
+    CORS(app)
     with app.app_context():
        db.create_all()
             
