f822c5371728cd15671444e02505d040a8a3b0fb
EmmaBia93 <emmabia2018@hotmail.com>
2024-06-23 12:08:28 -0300

    001: Delete Folder Server

--- server/app/models/__init__.py
+++
@@ -1,5 +0,0 @@
-#app //models/__init__.py
-
-from app.models.book import Book
-from app.models.author import Author
-from app.models.category import Category
--- server/scripts/comments.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE `Comments` (
-  `id` integer PRIMARY KEY,
-  `content` text NOT NULL,
-  `book_id` integer NOT NULL,
-  `created_at` datetime NOT NULL,
-  FOREIGN KEY (`book_id`) REFERENCES `Books` (`id`)
-);
